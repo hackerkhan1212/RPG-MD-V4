a45f44ad2960f29f5a037297ea7c81877e45ba8b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-11-25 17:33:21 +0500

    Update prem-animefeets.js

--- plugins/prem-animefeets.js
+++ plugins/prem-animefeets.js
@@ -2,8 +2,6 @@ import axios from "axios"
 let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
 let handler = async (m, {command, conn}) => {
 let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/animefeets?apikey${global.lolkeysapi}`)
-let json = res.data
-let url = json.url
 await conn.sendButton(m.chat, res, 'nsfw.jpg', `It is finished`, [
                 ['Next', `${usedPrefix + command}`],
                 ['Menu', `${usedPrefix}menu`]
