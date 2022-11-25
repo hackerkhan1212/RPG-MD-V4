4a2725a47709d6b4952a32e79fc9d197bd247f0b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-11-25 17:31:33 +0500

    Update prem-animebooty.js

--- plugins/prem-animebooty.js
+++ plugins/prem-animebooty.js
@@ -1,10 +1,16 @@
 import axios from "axios"
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
 let handler = async (m, {command, conn}) => {
 let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/animebooty?apikey=${global.lolkeysapi}`)
 let json = res.data
 let url = json.url
-conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, res, 'nsfw.jpg', `It is finished`, [
+                ['Next', `${usedPrefix + command}`],
+                ['Menu', `${usedPrefix}menu`]
+            ], m, fdoc)
+            
 }
+
 handler.help = ['animebooty']
 handler.tags = ['nsfw', 'premium']
 handler.command = /^(animebooty)$/i
