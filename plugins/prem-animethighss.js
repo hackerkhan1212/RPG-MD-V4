70e62f85edf5a52bd81da214364ab765343508a9
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-11-25 17:32:27 +0500

    Update prem-animethighss.js

--- plugins/prem-animethighss.js
+++ plugins/prem-animethighss.js
@@ -1,9 +1,12 @@
 import axios from "axios"
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
 let handler = async (m, {command, conn}) => {
 let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/animethighss?apikey=${global.lolkeysapi}`)
-let json = res.data
-let url = json.url
-conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, res, 'nsfw.jpg', `It is finished`, [
+                ['Next', `${usedPrefix + command}`],
+                ['Menu', `${usedPrefix}menu`]
+            ], m, fdoc)
+            
 }
 handler.help = ['animethighss']
 handler.tags = ['nsfw', 'premium']
