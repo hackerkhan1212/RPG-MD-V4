1d54d2bacda21c50c638a0831acb7d03c8f04549
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-11-25 17:30:59 +0500

    Update prem-animearm.js

--- plugins/prem-animearm.js
+++ plugins/prem-animearm.js
@@ -1,10 +1,15 @@
 import axios from "axios"
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
 let handler = async (m, {command, conn}) => {
 let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/animearmpits?apikey=${global.lolkeysapi}`)
-let json = res.data
-let url = json.url
 conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, res, 'nsfw.jpg', `It is finished`, [
+                ['Next', `${usedPrefix + command}`],
+                ['Menu', `${usedPrefix}menu`]
+            ], m, fdoc)
+            
 }
+
 handler.help = ['animearm']
 handler.tags = ['nsfw', 'premium']
 handler.command = /^(animearm)$/i
